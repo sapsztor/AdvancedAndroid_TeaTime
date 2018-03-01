db721a13b740fe3569ebe5eeb0e5ca44493e1d76
sapsztor <sapsztor@gmail.com>
2018-03-01 10:56:24 +0100

    All TODO completed

--- app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
@@ -16,16 +16,45 @@
 
 package com.example.android.teatime;
 
+
+import static android.app.Instrumentation.ActivityResult;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsNot.not;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 // TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(AndroidJUnit4.class)
 public class OrderSummaryActivityTest {
-
+    private static final String emailMessage = "I just ordered a delicious tea from TeaTime. Next time you are craving a tea, check them out!";
+    
     // TODO (2) Add the rule that indicates we want to use Espresso-Intents APIs in functional UI tests
-
+    @Rule
+    public IntentsTestRule<OrderSummaryActivity> mActivityRule = new IntentsTestRule<>(
+        OrderSummaryActivity.class);
 
     // TODO (3) Finish this method which runs before each test and will stub all external
     // intents so all external intents will be blocked
 
     public void stubAllExternalIntents() {
+        intending(not(isInternal())).respondWith(new ActivityResult(Activity.RESULT_OK, null));
 
     }
 
@@ -34,6 +63,11 @@ public class OrderSummaryActivityTest {
     // button matches the intent sent by the application
 
     public void clickSendEmailButton_SendsEmail() {
-
+        onView(withId(R.id.send_email_button)).perform(click());
+        // intended(Matcher<Intent> matcher) asserts the given matcher matches one and only one
+        // intent sent by the application.
+        intended(allOf(
+            hasAction(Intent.ACTION_SENDTO),
+            hasExtra(Intent.EXTRA_TEXT, emailMessage)));
     }
 }
